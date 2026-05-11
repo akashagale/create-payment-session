c3bf1cbd50e5485cc706e05b079e98ef5f9d777e
akashagale <akash.agale25@gmail.com>
2026-05-11 08:31:31 +0530

    initial setup

---
+++ src/test/java/com/htech/payments/PaymentValidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htech.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentValidationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
